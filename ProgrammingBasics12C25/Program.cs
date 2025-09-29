dd4f8a542701cb8e8f1a7617bb1c5c61d057c461
Al Stone <astone@heckgrammar.co.uk>
2025-09-29 15:07:42 +0100

    Up to date/time

--- ProgrammingBasics12C25/Program.cs
+++ ProgrammingBasics12C25/Program.cs
@@ -6,7 +6,47 @@ namespace ProgrammingBasics12C25
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("THis is a change");
+            // This is a comment
+            // Integer - Whole number
+            // (-2,147,483,648 to 2,147,483,647)
+            int myIntValue;
+            myIntValue = 42;
+
+            int anotherIntValue = 64;
+            Console.WriteLine(myIntValue);
+
+            // real 
+            float myFloat = 0.3F;
+            decimal myDecimal = 1.23456m;
+            double myDouble = 1.9876543;
+
+            // Boolean
+            bool myBoolean = false;
+            myBoolean = true;
+            myBoolean = !myBoolean; //flips the boolean value
+
+            // Character
+            char myChar = 'A';
+            char anotherChar;
+            anotherChar = '*';
+
+            // String
+            string myString = "This is a string";
+            string myString2;
+            myString2 = "";
+
+            // Date/Time
+            DateTime myDateTime = DateTime.Now;
+            Console.WriteLine(myDateTime);
+            Console.WriteLine(myDateTime.Year);
+            Console.WriteLine(myDateTime.Month);
+            Console.WriteLine(myDateTime.Day);
+            Console.WriteLine(myDateTime.Hour);
+            Console.WriteLine(myDateTime.Minute);
+            Console.WriteLine(myDateTime.Second);
+            Console.WriteLine(myDateTime.Millisecond);
+
+
         }
     }
 }
