e055b266d86dab2b44a39dafae1064a1cad2bca6
Al Stone <astone@heckgrammar.co.uk>
2025-09-29 14:38:53 +0100

    Demo Commit

--- ProgrammingBasics12C25/Program.cs
+++ ProgrammingBasics12C25/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ProgrammingBasics12C25
+﻿using System.Runtime.CompilerServices;
+
+namespace ProgrammingBasics12C25
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            Console.WriteLine("THis is a change");
         }
     }
 }
