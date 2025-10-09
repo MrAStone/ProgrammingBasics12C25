dd4f1484cbf1718b840db4529ad4b2898b1819f1
Mr Stone HGS <60425249+MrAStone@users.noreply.github.com>
2025-10-09 20:13:57 +0100

    Enhance comments and explanations in Program.cs
    Updated comments for clarity and added explanations for various programming constructs.

--- ProgrammingBasics12C25/Program.cs
+++ ProgrammingBasics12C25/Program.cs
@@ -1,59 +1,82 @@
-﻿using System.Runtime.CompilerServices;
+// This line imports a "namespace" called System.Runtime.CompilerServices.
+// Namespaces are like libraries of pre-written code. You don't need to worry about this specific one for now.
+using System.Runtime.CompilerServices;
 
+// A "namespace" is used to organize your code. It's like a folder for your program's files.
 namespace ProgrammingBasics12C25
 {
+    // A "class" is a blueprint for creating objects. Think of it as a container for your code,
+    // holding variables and functions (called "methods").
     internal class Program
     {
+        // A "struct" (short for structure) is a way to group several related variables into one place.
+        // It's like creating your own custom data type. Here, we're making one to hold information about a town.
         struct Town
         {
-            public string TownName;
-            public int Population;
-            public double Area;
-            public string County;
+            // These are the variables, or "fields," that every Town will have.
+            public string TownName; // A variable to store the town's name as text.
+            public int Population;   // A variable to store the population as a whole number.
+            public double Area;       // A variable to store the area, which can be a decimal number.
+            public string County;     // A variable to store the county name as text.
 
+            // This is a special method called a "constructor". It makes it easy to create a new Town
+            // and set all its values at once.
             public Town(string name, int pop, double tArea, string tCounty)
             {
+                // The "this" keyword refers to the current object being created.
+                // These lines assign the values passed into the constructor to the fields of the struct.
                 this.TownName = name;
                 this.Population = pop;
                 this.Area = tArea;
                 this.County = tCounty;
             }
         }
+
+        // This is the Main method. It's the starting point of every C# console application.
+        // When you run your program, the code inside these curly braces {} is the first to execute.
         static void Main(string[] args)
         {
-            // This is a comment
-            // Integer - Whole number
-            // (-2,147,483,648 to 2,147,483,647)
-            int myIntValue;
-            myIntValue = 42;
+            // A comment starts with //. The computer ignores these lines. They are for humans to read.
 
+            /*
+             * VARIABLES AND DATA TYPES
+             * A variable is like a named box in the computer's memory where you can store data.
+             */
+
+            // ## Integer ## (for whole numbers)
+            int myIntValue;    // This "declares" a variable named myIntValue that will hold an integer.
+            myIntValue = 42;   // This "assigns" the value 42 to the myIntValue variable.
+
+            // You can also declare and assign a value in a single line.
             int anotherIntValue = 64;
-            Console.WriteLine(myIntValue);
 
-            // real 
-            float myFloat = 0.3F;
-            decimal myDecimal = 1.23456m;
-            double myDouble = 1.9876543;
+            // Console.WriteLine() prints whatever is in the parentheses to the screen.
+            Console.WriteLine(myIntValue); // This will print the number 42.
 
-            // Boolean
-            bool myBoolean = false;
-            myBoolean = true;
-            myBoolean = !myBoolean; //flips the boolean value
+            // ## Real Numbers ## (for numbers with decimal points)
+            float myFloat = 0.3F;       // A 'float' is for decimal numbers. The 'F' is required.
+            decimal myDecimal = 1.23456m; // A 'decimal' is for high-precision numbers, like money. The 'm' is required.
+            double myDouble = 1.9876543;   // A 'double' is the most common type for decimal numbers.
 
-            // Character
-            char myChar = 'A';
+            // ## Boolean ## (for true or false values)
+            bool myBoolean = false; // A 'bool' can only be 'true' or 'false'.
+            myBoolean = true;       // You can change its value later.
+            myBoolean = !myBoolean; // The '!' symbol means NOT. It flips the value. 'true' becomes 'false'.
+
+            // ## Character ## (for a single letter, number, or symbol)
+            char myChar = 'A';      // A 'char' holds a single character. Note the single quotes ''.
             char anotherChar;
             anotherChar = '*';
 
-            // String
-            string myString = "This is a string";
+            // ## String ## (for text)
+            string myString = "This is a string"; // A 'string' holds a sequence of characters. Note the double quotes "".
             string myString2;
-            myString2 = "";
+            myString2 = ""; // An empty string.
 
-            // Date/Time
-            DateTime myDateTime = DateTime.Now;
-            Console.WriteLine(myDateTime);
-            Console.WriteLine(myDateTime.Year);
+            // ## Date/Time ##
+            DateTime myDateTime = DateTime.Now; // Gets the current date and time from your computer.
+            Console.WriteLine(myDateTime);      // Prints the full date and time.
+            Console.WriteLine(myDateTime.Year); // You can get specific parts of the date/time.
             Console.WriteLine(myDateTime.Month);
             Console.WriteLine(myDateTime.Day);
             Console.WriteLine(myDateTime.Hour);
@@ -61,115 +84,117 @@ namespace ProgrammingBasics12C25
             Console.WriteLine(myDateTime.Second);
             Console.WriteLine(myDateTime.Millisecond);
 
-
-            //pointer/reference
-            // not safe in C#
+            // ## Pointer/Reference ##
+            // A pointer stores a memory address instead of a value.
+            // This is an advanced topic and is generally not used in safe C# code, which is why it's commented out.
             // int* ptr = &x;
 
-            // records
-            // structure are defined outside the main program
-            Town myTown = new Town();
+            // ## Records (Using our Struct) ##
+            // Now we'll use the 'Town' struct we defined earlier.
+            Town myTown = new Town(); // Creates a new 'Town' object in memory.
+            // We assign values to the fields of our new town one by one.
             myTown.TownName = "Heckmondwike";
             myTown.Area = 35.4;
             myTown.Population = 12;
             myTown.County = "West Yorkshire";
-            Console.WriteLine(myTown.TownName);
+            Console.WriteLine(myTown.TownName); // Prints the name of the town.
+
+            // Here, we create another town using the constructor we made. It's a faster way to set all the values.
             Town anotherTown = new Town("Batley", 50, 12, "West Yorkshire");
 
-            // array
+            // ## Array ## (A list of items of the same type)
+            // This creates an array of strings and fills it with values immediately.
             string[] words = { "this", "is", "an", "array", "of", "strings" };
 
+            // This creates an array that can hold 10 integers, but it's currently empty.
             int[] nums = new int[10];
-            nums[0] = 5;
-            nums[9] = 24;
-            // nums[10] = 30; index 10 does not exist
+            // Array positions (called "indexes") start at 0. So the first item is at index 0.
+            nums[0] = 5;  // Puts the number 5 in the first position.
+            // The last item in a 10-item array is at index 9.
+            nums[9] = 24; // Puts 24 in the last position.
+            // nums[10] = 30; // This line would cause an error because index 10 doesn't exist (0-9 are the 10 spots).
 
-            // variable declaration
-            // datatype identifier (= value)
+
+            /*
+             * BASIC PROGRAMMING CONSTRUCTS
+             */
+
+            // ## Variable Declaration Recap ##
+            // Syntax: datatype identifier (= optional initial value);
             int aNum = 42;
-            string aString;
-            // constant declaration
-            // const datatype identifier = value
-            const int theAnswer = 42;
+            string aString; // Declared but not yet assigned a value.
 
-            // Assignment
-            // variable = value
+            // ## Constant Declaration ##
+            // A constant is a variable whose value cannot be changed after it's set.
+            const int theAnswer = 42;
+            // theAnswer = 43; // This would cause an error.
+
+            // ## Assignment ##
+            // This is the process of putting a value into a variable using the = sign.
             aString = "I am assigning this value to a variable";
 
-            // selection
-            // Uses if
-            // if(criteria){code}
-            if (aNum != 42)
+
+            // ## Selection ## (Making decisions in code)
+            // An 'if' statement runs code only if a certain condition is true.
+            // '!=' means "not equal to".
+            if (aNum != 42) // Is aNum not equal to 42? (This is false, since aNum is 42).
             {
+                // This code will be skipped because the condition was false.
                 Console.WriteLine("not 42");
             }
 
-            if(aNum < 40)
+            // An 'if-else' statement provides an alternative path if the 'if' condition is false.
+            if (aNum < 40) // Is aNum less than 40? (False).
             {
                 Console.WriteLine("less than 40");
             }
-            else
+            else // Since the 'if' was false, the 'else' block runs.
             {
                 Console.WriteLine("Not less than 40");
             }
 
-            if (aNum >= 50)
+            // You can chain them together with 'else if' to check multiple conditions.
+            if (aNum >= 50) // Is aNum greater than or equal to 50? (False).
             {
                 Console.WriteLine("Greater than or equal to 50");
             }
-            else if(aNum < 30)
+            else if (aNum < 30) // Is aNum less than 30? (False).
             {
                 Console.WriteLine("less than 30");
             }
-            else
+            else // Since all the above were false, this final 'else' runs.
             {
                 Console.WriteLine("Between 30 and 50");
             }
 
+            // A 'switch' statement is a clean way to check a variable against a list of possible values.
             switch (aNum)
             {
-                case 42:
+                case 42: // If aNum is 42...
                     Console.WriteLine("42");
-                    break;
-                case 43:
+                    break; // The 'break' keyword exits the switch statement. It's required.
+                case 43: // If aNum is 43...
                     Console.WriteLine("43");
                     break;
-                case > 50:
+                case > 50: // You can also check for conditions (if aNum is greater than 50...).
                     Console.WriteLine("Over 50");
                     break;
-                default:
+                default: // If none of the above cases match...
                     Console.WriteLine("None of the above");
                     break;
             }
 
             bool isItTrue = true;
-            if (!isItTrue)
+            // The '!' (NOT) operator is useful in conditions.
+            if (!isItTrue) // This reads as "if NOT isItTrue" or "if isItTrue is false".
             {
                 Console.WriteLine("no");
             }
 
+            // ## Iteration ## (Repeating code using loops)
 
-            // iteration
-            // definite iteration
-
-            for(int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(i);
-            }
-            for(int i = 0; i < 10; i += 3)
-            {
-                Console.WriteLine(i);
-            }
-            for(int i = 10; i > 0; i--)
-            {
-                Console.WriteLine(i);
-            }
-
-            string test = "HELLO WORLD";
-            for(int i = 0; i < test.Length; i++)
-            {
-                Console.WriteLine(test[i]);
-            }
-        }
-    }
-}
+            // A 'for' loop is used when you want to repeat code a specific number of times.
+            // It has three parts:
+            // 1. int i = 0;   (Initializer: Runs once at the very beginning)
+            // 2. i < 10;       (Condition: Checked before each repetition. The loop continues as long as this is true)
+            // 3. i++          (Iterator: Runs at the end of each repetition. i++ means "add 1 to i
